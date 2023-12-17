1959769b41765a67293f0f7fc2fc7470f00abc5d
maksimg3 <154068280+maksimg3@users.noreply.github.com>
2023-12-17 21:37:52 +0300

    Create prun.cpp

---
+++ obj/prun.cpp
@@ -0,0 +1,9 @@
+int main() {
+    // Запуск программы p1.cpp
+    system("g++ src/p1.cpp -o p1 && ./p1");
+
+    // Запуск программы p2.cpp
+    system("g++ src/p2.cpp -o p2 && ./p2");
+
+    return 0;
+}
