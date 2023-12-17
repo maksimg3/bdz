208063a2565725bbc9bbf6e89b5ce866594cbdf5
maksimg3 <154068280+maksimg3@users.noreply.github.com>
2023-12-17 22:45:24 +0300

    Update p2.cpp

--- src/p2.cpp
+++ src/p2.cpp
@@ -1,8 +1,38 @@
-#include "lib/function.h"
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <ctime>
+#include <fcntl.h>
 
-void monitoring()
-{
- 
- int status1 = std::system("top -b -H > ../files/log.txt"); // execute the UNIX command "ls -l >test.txt"
+int main() {
+    printf(" Результаты мониторинга системы успешно записаны в файл log.txt.\n");
+    // Открытие файла log.txt для записи
+    int fd = open("../files/log.txt", O_WRONLY | O_CREAT | O_TRUNC, 0644);
+    if (fd == -1) {
+        perror("open");
+        exit(1);
+    }
 
-} 
+    // Дублирование дескриптора файлового дескриптора stdout для записи в файл
+    if (dup2(fd, STDOUT_FILENO) == -1) {
+        perror("dup2");
+        exit(1);
+    }
+
+    // Отображение информации о системе
+    printf("System Monitoring");
+
+    // Код мониторинга системы
+    // Выведем информацию о текущем процессе и системном времени
+    int pid = getpid();
+    printf("Current process ID: %d", pid);
+    time_t current_time = time(NULL);
+    printf("Current time: %s", ctime(&current_time));
+
+    // Закрытие файла
+    close(fd);
+
+    return 0;
+}
