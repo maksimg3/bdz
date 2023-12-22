784f7586b61264066a089cfa21b4153bf5b0c345
maksimg3 <154068280+maksimg3@users.noreply.github.com>
2023-12-22 16:34:10 +0300

    Update p2.cpp

--- src/p2.cpp
+++ src/p2.cpp
@@ -5,6 +5,35 @@
 #include <sys/stat.h>
 #include <ctime>
 #include <fcntl.h>
+def monitor_memory():
+    memory_usage = psutil.virtual_memory().percent
+    with open("monitoring.txt", "a") as file:
+        file.write(f"Memory usage: {memory_usage}% at {time.strftime('%Y-%m-%d %H:%M:%S')}\n")
+
+
+def monitor_cpu():
+    cpu_usage = psutil.cpu_percent(0.1)
+    with open("monitoring.txt", "a") as file:
+        file.write(f"CPU usage: {cpu_usage}% at {time.strftime('%Y-%m-%d %H:%M:%S')}\n")
+
+
+def monitor_disk():
+    disk_usage = psutil.disk_usage('/').percent
+    with open("monitoring.txt", "a") as file:
+        file.write(f"Disk usage: {disk_usage}% at {time.strftime('%Y-%m-%d %H:%M:%S')}\n")
+
+
+def measure_performance():
+    # Some performance measurement code here
+    with open("monitoring.txt", "a") as file:
+        file.write(f"Performance measurement at {time.strftime('%Y-%m-%d %H:%M:%S')}\n")
+
+
+def detect_security_threats():
+    # Some security threat detection code here
+    with open("monitoring.txt", "a") as file:
+        file.write(f"Security threats detected at {time.strftime('%Y-%m-%d %H:%M:%S')}\n")
+
 
 int main() {
     printf(" Результаты мониторинга системы успешно записаны в файл log.txt.\n");
