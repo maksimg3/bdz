95a6fa8ab0e3640144782e4edebca6d213b301b9
maksimg3 <154068280+maksimg3@users.noreply.github.com>
2023-12-22 17:07:36 +0300

    Update prun.cpp

--- obj/prun.cpp
+++ obj/prun.cpp
@@ -1,9 +1,10 @@
+#include <cstdlib>
 int main() {
     // Запуск программы p1.cpp
-    system("g++ src/p1.cpp -o p1 && ./p1");
+    system("g++ ../src/p1.cpp -o p1 && ./p1");
 
     // Запуск программы p2.cpp
-    system("g++ src/p2.cpp -o p2 && ./p2");
+    system("g++ ../src/p2.cpp -o p2 && ./p2");
 
     return 0;
 }
